78c5a414ccf7427bfc37c70a0b3508e565416b1c
Ahmet Kutalmış Kesepara <110671833+Kuthaster@users.noreply.github.com>
2026-08-20 16:22:31 +0300

    BenchDTO naming fix

--- backend/cografi-nesne-takip/src/main/java/com/kutalmis/cografi_nesne_takip/Dto/BenchDTO.java
+++ backend/cografi-nesne-takip/src/main/java/com/kutalmis/cografi_nesne_takip/Dto/BenchDTO.java
@@ -6,7 +6,7 @@ import com.kutalmis.cografi_nesne_takip.entity.ObjectStatus;
 import com.kutalmis.cografi_nesne_takip.entity.ObjectType;
 
 public record BenchDTO(
-        Long id, ObjectType type, double latitude, double longitude, ObjectStatus status,
-        LocalDateTime createdAt, LocalDateTime updatedAt,
-        Integer seatCount, String material, Boolean hasBackRest) {
+                Long id, ObjectType type, double latitude, double longitude, ObjectStatus status,
+                LocalDateTime createdAt, LocalDateTime updatedAt,
+                Integer seatCount, String material, Boolean hasBackrest) {
 }
